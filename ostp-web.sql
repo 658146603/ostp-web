a416a1089a56cfb6cde7e5f2b4e02ce8cb0c5a72
wcf <153344693@qq.com>
2020-12-17 21:30:53 +0800

    sql build

---
+++ ostp-web.sql
@@ -0,0 +1,91 @@
+create database if not exists ostp character set utf8mb4 collate utf8mb4_bin;
+
+use ostp;
+
+
+create table college
+(
+    id   integer auto_increment not null primary key,
+    name varchar(32)            not null
+);
+
+create table major
+(
+    id      integer auto_increment not null primary key,
+    name    varchar(32)            not null,
+    college integer                not null,
+    foreign key (college) references college (id)
+);
+
+create table class
+(
+    id    integer auto_increment not null primary key,
+    name  varchar(32)            not null,
+    major integer                not null,
+    foreign key (major) references major (id)
+);
+
+
+create table student
+(
+    id       varchar(32) not null primary key,
+    name     varchar(32) not null,
+    class    integer     not null,
+    foreign key (class) references class (id),
+    password varchar(64) not null,
+    balance  decimal     not null,
+    email    varchar(64) null
+);
+
+create table teacher
+(
+    id      varchar(32) not null primary key,
+    name    varchar(32) not null,
+    college integer     not null,
+    foreign key (college) references college (id),
+    email   varchar(64) not null
+);
+
+create table book
+(
+    isbn  varchar(32) not null primary key,
+    name  varchar(64) not null,
+    price decimal     not null,
+    cover blob        not null # file or blob
+);
+
+create table course
+(
+    id    integer not null,
+    major integer not null,
+    foreign key (major) references major (id),
+    name  integer not null
+);
+
+create table semester
+(
+    id   integer auto_increment not null primary key,
+    year integer                not null,
+    no   integer                not null
+);
+
+create table course_open
+(
+    course   integer     not null,
+    foreign key (course) references course (id),
+    semester integer     not null,
+    foreign key (semester) references semester (id),
+    primary key (course, semester),
+    book     varchar(32) not null,
+    foreign key (book) references book (isbn),
+    teacher  varchar(32) not null,
+    foreign key (teacher) references teacher (id)
+);
+
+create table second_hand
+(
+    isbn     varchar(32) not null,
+    foreign key (isbn) references book (isbn),
+    price    decimal     not null,
+    exchange boolean     not null default false
+);
