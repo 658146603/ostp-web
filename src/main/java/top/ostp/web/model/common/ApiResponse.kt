6db698605b925b5d917a2f970de209c38942810a
h1542462994 <t1542462994@outlook.com>
2020-12-18 14:47:07 +0800

    继续完善Api返回值的代码

--- src/main/java/top/ostp/web/model/common/ApiResponse.kt
+++ src/main/java/top/ostp/web/model/common/ApiResponse.kt
@@ -1,6 +1,6 @@
 package top.ostp.web.model.common
 
-data class ApiResponse<T>(var code:Int, var summary:String, var message:String, var data: T? = null) {
+data class ApiResponse<T>(var code:Int, var summary:String, var message:String = "", var data: T? = null) {
     constructor(token: ResponseToken, message: String, data: T ?= null) : this(token.code, token.summary, message, data)
     constructor(token: ResponseToken, data: T? = null) : this(token.code, token.summary, token.defaultMessage, data)
 }
