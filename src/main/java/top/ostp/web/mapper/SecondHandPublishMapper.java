eeb2fdb634b228c9073187355013589600395e6e
DreamFish12450 <45327920+DreamFish12450@users.noreply.github.com>
2020-12-19 16:53:30 +0800

    seed-data

--- src/main/java/top/ostp/web/mapper/SecondHandPublishMapper.java
+++ src/main/java/top/ostp/web/mapper/SecondHandPublishMapper.java
@@ -10,16 +10,30 @@ import java.util.List;
 
 @Mapper
 public interface SecondHandPublishMapper {
-    @Insert("insert into second_hand_publish(person, book, price, exchange, status) values (#{person.id},#{book.id},#{price},#{exchange},#{status})")
+    @Insert("insert into second_hand_publish(person, book, price, exchange, status) values (#{person.id},#{book.isbn},#{price},#{exchange},#{status})")
     int insert(SecondHandPublish secondHandPublish);
 
     @Update("update second_hand_publish set book = #{book.id},price = #{price},exchange = #{exchange},status = #{status} where person = #{id}")
     int update(SecondHandPublish secondHandPublish);
 
     @Delete("delete from second_hand_publish where book = #{book.isbn}")
-    int deleteByBookISBN(Book book);
+
+    int deleteByBookISBN(@Param("book") Book book);
 //
     @Select("select * from second_hand_publish")
+    @Results(
+            value = {
+                    @Result(
+                            property = "person",column = "person",
+                            one = @One(select ="top.ostp.web.mapper.StudentMapper.selectStudentById",fetchType = FetchType.EAGER)
+                    ),
+                    @Result(
+                            property = "book",column = "book",
+                            one = @One(select = "top.ostp.web.mapper.BookMapper.selectByISBN",fetchType = FetchType.EAGER)
+                    )
+
+            }
+    )
     List<SecondHandPublish> selectAll();
 
     @Select("select * from second_hand_publish where person = #{id}")
--- src/test/java/top/ostp/web/SecondHandPublishTests.java
+++ src/test/java/top/ostp/web/SecondHandPublishTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import top.ostp.web.mapper.BookMapper;
 import top.ostp.web.mapper.SecondHandPublishMapper;
 import top.ostp.web.mapper.StudentMapper;
+import top.ostp.web.model.Book;
 import top.ostp.web.model.SecondHandPublish;
 import top.ostp.web.model.Student;
 import top.ostp.web.service.SecondHandPublishService;
@@ -46,6 +47,25 @@ public class SecondHandPublishTests {
             System.out.println(s1.toString());
 
         }
+    }
+    @Test
+    public void insertAndDelete(){
+        Student s = new Student();
+        s.setId("201806060231");
+        Book book = new Book();
+        book.setIsbn("9787-212-222-333");
+        SecondHandPublish secondHandPublish = new SecondHandPublish(s,book,12,0,1);
+//        secondHandPublishMapper.insert(secondHandPublish);
+//        secondHandPublishMapper.deleteByBookISBN(book);
+    }
+    @Test
+    public void update(){
+        Student s = new Student();
+        s.setId("201806060231");
+        Book book = new Book();
+        book.setIsbn("9787-212-222-423");
+        SecondHandPublish secondHandPublish = new SecondHandPublish(s,book,12,0,1);
+
     }
 
 }
